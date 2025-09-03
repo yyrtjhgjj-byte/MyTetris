063c4848ce202c4726a89b013bb36c6d70b15604
yyrtjhgjj-byte <yyrtjhgjj@gmail.com>
2025-09-04 01:09:41 +0900

    Update GameTimer.swift

--- Sources/MyTetris/GameTimer.swift
+++ Sources/MyTetris/GameTimer.swift
@@ -1,1 +1,24 @@
+// このファイルも新しく作成します
+import TokamakShim
+import JavaScriptKit
 
+// ContentViewからこのクラスを丸ごと移動します
+class GameTimer: ObservableObject {
+    var timer: JSValue? = nil
+
+    func start(action: @escaping () -> Void) {
+        stop()
+        timer = JSObject.global.setInterval.function?(
+            JSClosure { _ in
+                action()
+                return .undefined
+            }, 1000)
+    }
+
+    func stop() {
+        if let timer = timer {
+            _ = JSObject.global.clearInterval.function?(timer)
+            self.timer = nil
+        }
+    }
+}
