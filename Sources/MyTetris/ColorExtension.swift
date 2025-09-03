5bb0026b161b321020eebb2e3f07835b70dc81d3
yyrtjhgjj-byte <yyrtjhgjj@gmail.com>
2025-09-04 01:09:11 +0900

    Create ColorExtension.swift

---
+++ Sources/MyTetris/ColorExtension.swift
@@ -0,0 +1,8 @@
+// このファイルを新しく作成します
+import TokamakShim
+
+// ContentViewからこの部分を丸ごと移動します
+extension Color {
+    static let cyan = Color(red: 0.0, green: 1.0, blue: 1.0)
+    static let purple = Color(red: 0.5, green: 0, blue: 0.5)
+}
