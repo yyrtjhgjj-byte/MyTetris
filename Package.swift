05b85ac65ee1cca0b8a410e0528a08381ea6e570
yyrtjhgjj-byte <yyrtjhgjj@gmail.com>
2025-09-03 23:43:02 +0900

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "MyTetris", // あなたのリポジトリ名に合わせて変えてもOK
+    platforms: [.macOS(.v12), .iOS(.v15)], // ネイティブ用の設定（今回は使わない）
+    dependencies: [
+        .package(url: "https://github.com/TokamakUI/Tokamak", from: "0.11.0")
+    ],
+    targets: [
+        .executableTarget(
+            name: "MyTetris", // 上のnameと同じ名前にする
+            dependencies: [
+                .product(name: "TokamakShim", package: "Tokamak")
+            ]),
+    ]
+)
